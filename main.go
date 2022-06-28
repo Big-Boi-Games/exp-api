db89dca163624ab7068679f1e1ed1ca0999d5bcd
Jay Underwood <jordan.u@yahoo.com>
2022-06-28 21:31:24 +0100

    Configured envars and drafted auth middleware.

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	Mid "exp-api/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
 	router := gin.Default()
+
 	router.GET("/status", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"Status": "All is well.",
 		})
 	})
+
+	user := router.Group("/user")
+	user.Use(Mid.AuthHandler())
+	{
+		user.GET("/status", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"Status": "Bingus.",
+			})
+		})
+	}
+
 	router.Run()
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func AuthHandler() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		godotenv.Load(".env")
+		tester := os.Getenv("AUTH_HEADER")
+		token := c.Request.Header.Get("Authorisation")
+
+		if len(token) == 0 || token != tester {
+			c.Abort()
+			c.JSON(430, gin.H{"Error": "You are not authorised."})
+		}
+
+	}
+}
